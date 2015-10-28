b7d3af44bbaf71bdbf103eacf5b40ba72cadb1c2
luiz.nazari <luiz.nazari@camtwo.com.br>
2015-10-28 18:04:22 -0200

    treinamento [testes loco]

--- src/huehue/br/rede/modelo/MultilayerPerceptron2.java
+++ src/huehue/br/rede/modelo/MultilayerPerceptron2.java
@@ -16,28 +16,28 @@ import org.encog.ml.data.basic.BasicMLData;
 
 @Getter
 public class MultilayerPerceptron2 extends JdvRedeAbstrata {
-
+	
 	public MultilayerPerceptron2(Integer numeroEntradas, Integer numeroSaidas, String nome) {
 		super(numeroEntradas, numeroSaidas, nome);
 	}
-
+	
 	public MultilayerPerceptron2() {
 		this(null);
 	}
-
+	
 	public MultilayerPerceptron2(String nome) {
 		this(9, 9, nome);
-
+		
 		momentum = 0.4;
 		margemDeErro = 0.06;
 		constanteDeAprendizagem = 0.05;
 	}
-
+	
 	@Override
 	public String getEstruturaRede() {
 		return "?:B->SIGMOID->18:B->SIGMOID->?";
 	}
-
+	
 	/**
 	 * {@inheritDoc}<br>
 	 * <br>
@@ -47,52 +47,52 @@ public class MultilayerPerceptron2 extends JdvRedeAbstrata {
 	@Override
 	public int traduzirSaida(MLData saida) {
 		List<MapaSaida> listaSaida = JdvUtils.RNA.toSaidasMapeadas(saida.getData());
-
+		
 		return buscaPrimeiroIndiceValido(listaSaida);
 	}
-
+	
 	@Override
 	public MLData convertePosicaoTabuleiroEmSaida(int posicao) {
 		double[] dd = new double[9];
-
+		
 		dd[posicao] = Caractere.X.getValor();
-
+		
 		return new BasicMLData(dd);
 	}
-
+	
 	private int buscaPrimeiroIndiceValido(List<MapaSaida> listaSaida) {
 		MapaSaida ms = listaSaida.get(0);
-
+		
 		if (entradasTmp[ms.index] == Caractere.VAZIO.getValor())
 			return ms.index;
-
+		
 		System.err.println("Computou um resultado inválido! " + ms
 				+ "\tEntradas: " + JdvLog.tabuleiro(entradasTmp));
 		listaSaida.remove(0);
 		return buscaPrimeiroIndiceValido(listaSaida);
 	}
-
+	
 	public static void main(String[] args) {
-		JdvUtils.Arquivo.versionamento(2);
-		JdvRedeAbstrata rede = new MultilayerPerceptron2("treinamento").inicializar();
+		JdvUtils.Arquivo.versionamento(1);
+		JdvRedeAbstrata rede = new MultilayerPerceptron2("treinamento2").inicializar();
 		ConjuntosDados dados = JdvUtils.Arquivo.carregarDados(rede);
 		MLDataSet setDados = dados.getConjuntosMLSet();
-
+		
 		rede.treinar(dados);
-
+		
 		JdvUtils.Arquivo.incrementaVersao();
 		JdvUtils.Arquivo.salvarRede(rede);
 		JdvUtils.Arquivo.salvarDados(rede, dados);
-
+		
 //		rede.testar(setDados.get(( int ) (Math.random() * setDados.size())));
 		rede.testar(setDados);
-
+		
 //		rede.testar(new BasicMLDataPair(new BasicMLData(new double[] {
 //			-1, 0, -1, -1, 1, 1, 1, -1, 1
 //		}), new BasicMLData(new double[] {
 //			0, 1, 0, 0, 0, 0, 0, 0, 0
 //		})));
-
+		
 		Encog.getInstance().shutdown();
 	}
 }
